a8e23faa1661042d37aa32c01d99d173a9ea9477
JohnnyT <johnnyt@moneydesktop.com>
2013-04-09 18:57:59 -0600

    Add date_dimension, user_dimension, count_measure, person_count_measure

--- lib/rubiks/nodes/cube.rb
+++ lib/rubiks/nodes/cube.rb
@@ -6,6 +6,11 @@ require 'rubiks/nodes/calculated_member'
 module ::Rubiks
 
   class Cube < ::Rubiks::AnnotatedNode
+    value :date_dimension, String
+    value :person_dimension, String
+    value :count_measure, String
+    value :person_count_measure, String
+
     child :dimensions, [::Rubiks::Dimension]
     child :measures, [::Rubiks::Measure]
     child :calculated_members, [::Rubiks::CalculatedMember]
@@ -13,7 +18,7 @@ module ::Rubiks
     validates :dimensions_present, :measures_present, :calculated_members_if_present
 
     def self.new_from_hash(hash={})
-      new_instance = new('',[],[],[])
+      new_instance = new('','','','','',[],[],[])
       return new_instance.from_hash(hash)
     end
 
@@ -22,10 +27,38 @@ module ::Rubiks
       working_hash.stringify_keys!
 
       parse_name(working_hash.delete('name'))
+      parse_date_dimension(working_hash.delete('date_dimension'))
+      parse_person_dimension(working_hash.delete('person_dimension'))
+      parse_count_measure(working_hash.delete('count_measure'))
+      parse_person_count_measure(working_hash.delete('person_count_measure'))
       parse_dimensions(working_hash.delete('dimensions'))
       parse_measures(working_hash.delete('measures'))
       parse_calculated_members(working_hash.delete('calculated_members'))
       return self
+    end
+
+    def parse_date_dimension(input_value)
+      return if input_value.nil?
+
+      self.date_dimension = input_value.to_s.underscore
+    end
+
+    def parse_person_dimension(input_value)
+      return if input_value.nil?
+
+      self.person_dimension = input_value.to_s.underscore
+    end
+
+    def parse_count_measure(input_value)
+      return if input_value.nil?
+
+      self.count_measure = input_value.to_s.underscore
+    end
+
+    def parse_person_count_measure(input_value)
+      return if input_value.nil?
+
+      self.person_count_measure = input_value.to_s.underscore
     end
 
     def calculated_members_if_present
@@ -90,6 +123,10 @@ module ::Rubiks
       hash['dimensions'] = self.dimensions.map(&:to_hash) if self.dimensions.present?
       hash['measures'] = self.measures.map(&:to_hash) if self.measures.present?
       hash['calculated_members'] = self.calculated_members.map(&:to_hash) if self.calculated_members.present?
+      hash['date_dimension'] = self.date_dimension if self.date_dimension.present?
+      hash['person_dimension'] = self.person_dimension if self.person_dimension.present?
+      hash['count_measure'] = self.count_measure if self.count_measure.present?
+      hash['person_count_measure'] = self.person_count_measure if self.person_count_measure.present?
 
       return hash
     end
