3e2ebbd0381402d583c7f57e5f7d20d39a1e8e55
JohnnyT <johnnyt@moneydesktop.com>
2013-04-10 18:07:56 -0600

    Add yard, coveralls; Clean up Rakefile and spec_helper

--- Gemfile
+++ Gemfile
@@ -6,14 +6,15 @@ gemspec
 # Test and development dependencies are defined here
 # so CI can include just test dependencies
 group :test do
-  gem 'rake'
+  gem 'coveralls', :require => false
+  gem 'simplecov', :require => false
   gem 'rspec'
-  gem 'rspec-pride'
 end
 
 group :development do
   gem 'awesome_print'
+  gem 'kramdown'
   gem 'pry'
   gem 'pry-nav'
-  gem 'simplecov'
+  gem 'yard'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,37 @@
-lib_path = File.expand_path("../../lib", __FILE__)
-$LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
+#lib_path = File.expand_path("../../lib", __FILE__)
+#$LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
+#
+## Don't use simplecov in ruby 1.8
+#if RUBY_VERSION !~ /^1\.8/
+#  begin
+#    require 'simplecov'
+#    SimpleCov.start do
+#      add_filter '/spec/'
+#    end
+#  rescue LoadError
+#  end
+#end
+#
+#require 'bundler'
+#Bundler.require(:default, :development, :test)
+#
+#require 'rubiks'
+#
+#RSpec.configure do |config|
+#  config.include Matchers
+#end
 
-# Don't use simplecov in ruby 1.8
-if RUBY_VERSION !~ /^1\.8/
-  begin
-    require 'simplecov'
-    SimpleCov.start do
-      add_filter '/spec/'
-    end
-  rescue LoadError
-  end
-end
+require 'simplecov'
+require 'coveralls'
 
-require 'bundler'
-Bundler.require(:default, :development, :test)
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
 
 require 'rubiks'
+require 'rspec'
 
 require 'support/schema_context'
 require 'support/matchers/be_like'
