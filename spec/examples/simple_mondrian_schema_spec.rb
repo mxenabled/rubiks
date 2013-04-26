20e03b5e6dc930d2fa8cc0cb11e2453a44ac3eef
JohnnyT <johnnyt@moneydesktop.com>
2013-04-26 14:48:42 -0600

    Update example spec

--- spec/examples/simple_mondrian_schema_spec.rb
+++ spec/examples/simple_mondrian_schema_spec.rb
@@ -2,41 +2,54 @@ require 'spec_helper'
 
 describe 'A simple Mondrian schema' do
   subject {
-    ::Rubiks::Schema.define do
-      name :sales
-
-      cube :sales do
-        name :sales
+    ::Rubiks::Schema.define :banking do
+      cube :transactions do
         dimension :date, :type => 'TimeDimension' do
-          hierarchy :year_quarter_month do
-            level :year, :type => :numeric, :level_type => 'TimeYears'
-            level :quarter, :type => :string, :level_type => 'TimeQuarters'
-            level :month, :type => :numeric, :level_type => 'TimeMonths'
+          hierarchy :yqmwd, :caption => 'YQMWD' do
+            level :year,    :level_type => 'TimeYears', :type => :numeric, :contiguous => true
+            level :quarter, :level_type => 'TimeQuarters', :type => :numeric, :contiguous => true, :cardinality => :low
+            level :month,   :level_type => 'TimeMonths', :type => :numeric, :contiguous => true
+            level :week,    :level_type => 'TimeWeeks', :type => :numeric, :column => :week_of_month, :contiguous => true, :cardinality => :low
+            level :day,     :level_type => 'TimeDays', :type => :numeric, :contiguous => true
           end
         end
-        measure :sales, :aggregator => 'sum', :format_string => '$#,###'
-        measure :cost, :aggregator => 'sum', :format_string => '$#,###'
-        calculated_measure :profit, :formula => '[Measures].[Sales] - [Measures].[Cost]', :format_string => '$#,###'
-      end
 
+        dimension :account do
+          hierarchy :account_type do
+            level :asset_liability, :cardinality => :low
+            level :account_type
+          end
+        end
+
+        measure :count, :column => :quantity
+        measure :amount, :aggregator => :sum, :format_string => '$#,###'
+      end
     end
   }
 
   its(:to_xml) { should be_equivalent_to(Nokogiri::XML(<<-XML)) }
-    <schema name="Sales">
-      <cube name="Sales">
-        <table name="view_sales"/>
+    <schema name="Banking">
+      <cube name="Transactions">
+        <table name="view_transactions"/>
         <dimension name="Date" foreignKey="date_id" type="TimeDimension">
-          <hierarchy name="Year Quarter Month" primaryKey="id" hasAll="true">
+          <hierarchy name="YQMWD" hasAll="true" primaryKey="id">
             <table name="view_dates"/>
-            <level name="Year" column="year" type="Numeric" levelType="TimeYears"/>
-            <level name="Quarter" column="quarter" type="String" levelType="TimeQuarters"/>
-            <level name="Month" column="month" type="Numeric" levelType="TimeMonths"/>
+            <level name="Year" column="year" levelType="TimeYears" type="Numeric"/>
+            <level name="Quarter" column="quarter" levelType="TimeQuarters" type="Numeric"/>
+            <level name="Month" column="month" levelType="TimeMonths" type="Numeric"/>
+            <level name="Week" column="week_of_month" levelType="TimeWeeks" type="Numeric"/>
+            <level name="Day" column="day" levelType="TimeDays" type="Numeric"/>
           </hierarchy>
         </dimension>
-        <measure name="Sales" aggregator="sum" column="sales" formatString="$#,###"/>
-        <measure name="Cost" aggregator="sum" column="cost" formatString="$#,###"/>
-        <calculatedMember name="Profit" dimension="Measures" formula="[Measures].[Sales] - [Measures].[Cost]" formatString="$#,###"/>
+        <dimension name="Account" foreignKey="account_id">
+          <hierarchy name="Account Type" hasAll="true" primaryKey="id">
+            <table name="view_accounts"/>
+            <level name="Asset Liability" column="asset_liability"/>
+            <level name="Account Type" column="account_type"/>
+          </hierarchy>
+        </dimension>
+        <measure name="Count" column="quantity" aggregator="count"/>
+        <measure name="Amount" column="amount" aggregator="sum" formatString="$#,###"/>
       </cube>
     </schema>
   XML
